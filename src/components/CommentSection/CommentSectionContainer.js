adeb908b4e0048ff7a0a1096d9a35f455591ecce
jimmoua1 <tsheejmaus@yahoo.com>
2020-02-28 17:51:32 -0800

    some files have issues

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,6 +6,7 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const { comments } = props;
   const [newComments, setComments] = useState(comments);
 
   return (
